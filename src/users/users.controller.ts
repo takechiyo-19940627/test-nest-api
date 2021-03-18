5515eed2080beb28493e35455bfeae7cea204650
takehiro <45512992+takechiyo-19940627@users.noreply.github.com>
2021-03-18 21:32:33 +0900

    feat: user 登録処理をControllerに追加しました

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,41 @@
-import { Controller } from '@nestjs/common';
+import {
+  Controller,
+  HttpCode,
+  HttpStatus,
+  Post,
+  Body,
+  HttpException,
+} from '@nestjs/common';
+import { CreateUserDTO } from './users.dto';
+import { UsersService } from './users.service';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+  constructor(private readonly usersService: UsersService) {}
+
+  @Post('register')
+  @HttpCode(HttpStatus.CREATED)
+  async createUser(@Body() createUserDTO: CreateUserDTO) {
+    if (this.usersService.findUserByScreenName(createUserDTO.screenName)) {
+      throw new HttpException(
+        {
+          status: HttpStatus.CONFLICT,
+          error: `Screen name '${createUserDTO.screenName}' is already taken.`,
+        },
+        409,
+      );
+    }
+    try {
+      await this.usersService.register(createUserDTO);
+    } catch (e) {
+      throw new HttpException(
+        {
+          status: HttpStatus.INTERNAL_SERVER_ERROR,
+          error: 'Internal Server Error',
+        },
+        500,
+      );
+    }
+    return;
+  }
+}
